cc4430dee91630f6a3a65d3b371885c4a851a731
Arthur Ming <guobin.ming@pwc.com>
2023-05-09 15:50:00 +0800

    feat: process button pressed event

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -41,8 +46,8 @@ class MyHomePage extends StatelessWidget {
           Text('A random daming idea:'), 
           Text(appState.current.asLowerCase),
           ElevatedButton(
-            onPressed: () => {
-              print('button pressed!')
+            onPressed: () {
+              appState.getNext();
             },
             child: Text('Next'),
           )
