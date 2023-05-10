517da85eea88e9e4e4731ca56cd07ddeae624dbb
Arthur Ming <guobin.ming@pwc.com>
2023-05-10 19:45:45 +0800

    feat: add toggleFavorite feature

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,17 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -41,6 +52,17 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    var iconText;
+
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+      iconText = 'Like';
+    } else {
+      icon = Icons.favorite_border;
+      iconText = 'Like';
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -48,11 +70,24 @@ class MyHomePage extends StatelessWidget {
           children: [
             BigCard(pair: pair),
             SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text(iconText),
+                ),
+                SizedBox(width: 10,),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
             )
           ]),
       ),
